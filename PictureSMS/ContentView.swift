5bbc24a2315c09df9ff489806b68cb648eed08db
aungkomin <jonahaung@gmail.com>
2021-04-02 19:26:31 +0800

    Initial Commit

---
+++ PictureSMS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PictureSMS
+//
+//  Created by Aung Ko Min on 2/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PictureSMS/PictureSMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictureSMSApp.swift
+//  PictureSMS
+//
+//  Created by Aung Ko Min on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct PictureSMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
